b7921e96cb017db3892c6aa8d4c3c45cb8f1394c
Josiah Nunemaker <josiah.nunemaker@gmail.com>
2025-02-14 10:38:27 -0500

    Test mod-tap without U_MT macro

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -9,6 +9,6 @@
 
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,                                      &kp Y,                                &kp U,             &kp I,             &kp O,             &kp P,             \
-U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp U_MT(LG(LA(LC(LSHFT))), G),             &kp U_MT(RG(RA(RC(RSHFT))), H),       U_MT(RSHFT, J),    U_MT(RCTRL, K),    U_MT(RALT, L),     U_MT(RGUI, SQT),   \
+U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &mt LG(LA(LC(LSHFT))) G,                    &mt RG(RA(RC(RSHFT))) H,       U_MT(RSHFT, J),    U_MT(RCTRL, K),    U_MT(RALT, L),     U_MT(RGUI, SQT),   \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,                                      &kp N,                                &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),                         U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
